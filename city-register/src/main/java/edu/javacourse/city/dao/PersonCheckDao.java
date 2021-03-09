ac8176f05e3b3553b0f879ad352f56eaa88ec774
Dima Maloshtan <maloshtanda@gmail.com>
2021-03-09 23:12:26 +0300

    add web files

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -6,37 +6,71 @@ import edu.javacourse.city.exception.PersonCheckException;
 
 import java.sql.*;
 
-public class PersonCheckDao
-{
+public class PersonCheckDao {
     private static final String SQL_REQUEST =
             "select temporal from cr_address_person ap " +
                     "inner join cr_person p on p.person_id = ap.person_id " +
                     "inner join cr_address a on a.address_id = ap.address_id " +
                     "where " +
-                    "where "+
+                    "CURRENT_DATE >= ap.start_date and (CURRENT_DATE <= ap.end_date or ap.end_date is null)" +
                     "and upper(p.sur_name COLLATE \"C\") = upper(? COLLATE \"C\")  " +
                     "and upper(p.given_name COLLATE \"C\") = upper(? COLLATE \"C\")  " +
                     "and upper(patronymic COLLATE \"C\") = upper(? COLLATE \"C\")  " +
                     "and p.date_of_birth = ? " +
                     "and a.street_code = ?  " +
-                    "and upper(a.building COLLATE \"C\") = upper(? COLLATE \"C\") " +
-                    "and upper(extension COLLATE \"C\") = upper(? COLLATE \"C\")  " +
-                    "and upper(a.apartment COLLATE \"C\") = upper(? COLLATE \"C\")";
+                    "and upper(a.building COLLATE \"C\") = upper(? COLLATE \"C\") ";
+
+
+//    public PersonCheckDao(){
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+
+
+    private ConnectionBuilder connectionBuilder;
+
+    public void setConnectionBuilder(ConnectionBuilder connectionBuilder) {
+        this.connectionBuilder = connectionBuilder;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return connectionBuilder.getConnection();
+    }
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
 
-        try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(SQL_REQUEST)) {
+        String sql = SQL_REQUEST;
+        if (request.getExtension() != null) {
+            sql += "and upper(a.extension COLLATE \"C\") = upper(? COLLATE \"C\")  ";
+        } else {
+            sql += "and extension is null ";
+        }
+        if (request.getApartment() != null) {
+            sql += "and upper(a.apartment COLLATE \"C\") = upper(? COLLATE \"C\") ";
+        } else {
+            sql += "and a.apartment is null ";
+        }
 
-            stmt.setString(1, request.getSurName());
-            stmt.setString(2, request.getGivenName());
-            stmt.setString(3, request.getPatronymic());
-            stmt.setDate(4, java.sql.Date.valueOf(request.getDateOfBirth()));
-            stmt.setInt(5, request.getStreetCode());
-            stmt.setString(6, request.getBuilding());
-            stmt.setString(7, request.getExtension());
-            stmt.setString(8, request.getApartment());
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            int count = 1;
+            stmt.setString(count++, request.getSurName());
+            stmt.setString(count++, request.getGivenName());
+            stmt.setString(count++, request.getPatronymic());
+            stmt.setDate(count++, java.sql.Date.valueOf(request.getDateOfBirth()));
+            stmt.setInt(count++, request.getStreetCode());
+            stmt.setString(count++, request.getBuilding());
+            if (request.getExtension() != null) {
+                stmt.setString(count++, request.getExtension());
+            }
+            if (request.getApartment() != null) {
+                stmt.setString(count++, request.getApartment());
+            }
 
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
@@ -50,8 +84,4 @@ public class PersonCheckDao
         return response;
     }
 
-    private Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:postgresql://localhost/city_register", "postgres", "postgres");
-
-    }
 }
